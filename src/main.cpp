603bad1af99bfa0e1d96ab60b3b3c3e6db22b6bc
Damy van Valenberg <damyvv@hotmail.com>
2017-10-04 17:34:34 +0200

    Simple triangle using glew

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,43 @@
 #include "InMineConfig.h"
-#include <iostream>
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 #include "Window.h"
 
 int main() {
 	std::cout << "InMine version: " << InMine_VERSION_STRING << std::endl;
 
-	Window window(800, 600, false, "InMine " + std::string(InMine_VERSION_STRING));
+	float vertices[] = {
+		0.0, 0.5,
+		0.5, -0.5,
+		-0.5, -0.5,
+	};
 
-	glClearColor(1, 0, 0, 1);
-	while (window.isOpen()) {
-		glClear(GL_COLOR_BUFFER_BIT);
+	try {
+		Window window(800, 600, false, "InMine " + std::string(InMine_VERSION_STRING));
 
-		window.update();
+		glEnableVertexAttribArray(0);
+		GLuint vbo;
+		glCreateBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, vertices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glClearColor(1, 0, 0, 1);
+		while (window.isOpen()) {
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+
+			window.update();
+		}
+	} catch (std::exception e) {
+		std::cout << e.what() << std::endl;
+		getchar();
+		return -1;
 	}
 
 	return 0;
