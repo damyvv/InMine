62be23bba3e8d4aef4f9cf1bc3c32f8acc89a078
Damy van Valenberg <damyvv@hotmail.com>
2017-10-07 23:32:32 +0200

    Shaders now accept colors

--- res/shaders/defaultFragmentShader.glsl
+++ res/shaders/defaultFragmentShader.glsl
@@ -1,5 +1,7 @@
 #version 330
 
+in vec4 frag_col;
+
 void main() {
-    gl_FragColor = vec4(0, 1, 1, 1);
+    gl_FragColor = frag_col;
 }
--- res/shaders/defaultVertexShader.glsl
+++ res/shaders/defaultVertexShader.glsl
@@ -1,10 +1,14 @@
 #version 330
 
 layout(location = 0) in vec3 pos;
+layout(location = 1) in vec4 col;
 
 uniform mat4 modelMatrix;
 uniform mat4 projectionMatrix;
 
+out vec4 frag_col;
+
 void main() {
+	frag_col = col;
     gl_Position = projectionMatrix * modelMatrix * vec4(pos, 1);
 }
