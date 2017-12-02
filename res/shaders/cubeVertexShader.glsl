83110edcd4f08391cbedc7b017c2106cf07cfa7f
Damy van Valenberg <damyvv@hotmail.com>
2017-12-02 22:16:20 +0100

    Changed to glvertexattribdivisor

--- res/shaders/cubeVertexShader.glsl
+++ res/shaders/cubeVertexShader.glsl
@@ -4,13 +4,10 @@ layout(location = 0) in vec3 pos;
 layout(location = 1) in vec4 col;
 layout(location = 2) in vec2 uv;
 
+layout(location = 3) in vec3 offset;
+
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
-
-layout(std430, binding=2) buffer OffsetsBlock
-{
-	float offsets[];
-};
 
 out vec4 frag_col;
 out vec2 frag_uv;
@@ -18,6 +15,5 @@ out vec2 frag_uv;
 void main() {
 	frag_col = col;
 	frag_uv = uv;
-	vec3 offset = vec3(offsets[gl_InstanceID * 3], offsets[gl_InstanceID * 3 + 1], offsets[gl_InstanceID * 3 + 2]);
     gl_Position = projectionMatrix * viewMatrix * vec4(pos + offset, 1);
 }
--- src/graphics/renderer/CubeRenderer.cpp
+++ src/graphics/renderer/CubeRenderer.cpp
@@ -9,10 +9,9 @@ CubeRenderer::CubeRenderer(const glm::mat4& projectionMatrix, const Camera* cam)
 	m_Cubes.reserve(BATCH_SIZE);
 
 	glGenBuffers(1, &m_OBO);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_OBO);
-	glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(float) * 4 * BATCH_SIZE, nullptr, GL_DYNAMIC_DRAW);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, m_OBO);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, m_OBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * BATCH_SIZE, nullptr, GL_STREAM_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 CubeRenderer::~CubeRenderer() {
@@ -30,17 +29,26 @@ void CubeRenderer::render() {
 	m_Shader->start();
 	m_Shader->setViewMatrix(m_Camera->getViewMatrix());
 
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_OBO);
-	glm::vec3* offsetsPtr = (glm::vec3*)glMapBuffer(GL_SHADER_STORAGE_BUFFER, GL_WRITE_ONLY);
+	const Renderable3D* r = m_Cubes[0]->getRenderable3D();
+	r->bind();
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_OBO);
+
+	static bool options_set = false;
+	if (!options_set) {
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glVertexAttribDivisor(3, 1);
+		options_set = true;
+	}
+
+	glm::vec3* offsetsPtr = (glm::vec3*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
 
 	for (int i = 0; i < m_Cubes.size(); i++) {
 		*offsetsPtr++ = m_Cubes[i]->getPosition();
 	}
 
-	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-
-	const Renderable3D* r = m_Cubes[0]->getRenderable3D();
-	r->bind();
+	glUnmapBuffer(GL_ARRAY_BUFFER);
 
 	glDrawElementsInstanced(GL_TRIANGLES, r->getIndicesCount(), GL_UNSIGNED_BYTE, 0, m_Cubes.size());
 
